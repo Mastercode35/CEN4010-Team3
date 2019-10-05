29947a79e20a4e1f0a7830f0cc76e852f4531bc4
Kevin De Jesus <38739351+gizaom@users.noreply.github.com>
2019-10-05 16:22:15 -0400

    Update views.py
    created view of the books table

--- GeekText/bookstore/views.py
+++ GeekText/bookstore/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.edit import FormView
 
-# Create your views here.
+def feed(request):
+    posts = Book.objects.all()
+    return render(request, 'feed.xml', {'posts': posts})
 
 def index(request):
     return HttpResponse("Hello, you are viewing the BookStore index.")
