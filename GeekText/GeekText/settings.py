4daca205ce59914427c3b925574d644d5872e5ba
mastercode35 <williamelias3@hotmail.com>
2019-10-05 20:23:59 -0400

    Made the Database locally

--- GeekText/GeekText/settings.py
+++ GeekText/GeekText/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'GeekText.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookstore_website_project',
+        'USER':'team3',
+        'PASSWORD':'password',
+        'PORT':'5432',
+        'HOST': 'localhost',
     }
 }
 
