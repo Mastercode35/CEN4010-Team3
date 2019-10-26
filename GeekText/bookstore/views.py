b53fda7890fe7ec6a18db24888a1ce4076f64d96
Mastercode35 <Williamelias3@hotmail.com>
2019-10-26 18:40:47 -0400

    Added multiple functionalities to the Book Search Page. Also added a Top Sellers Page. Both pages still under construction.

--- GeekText/bookstore/views.py
+++ GeekText/bookstore/views.py
@@ -18,7 +18,7 @@ def feed(request):
 
 def book_search(request):
     books_list = Book.objects.raw(
-            'SELECT * FROM bookstore_book b, bookstore_wrote w, bookstore_author a WHERE (w.author_id = a.id) AND (w.book_id = b.id)'
+            'SELECT * FROM bookstore_book b, bookstore_wrote w, bookstore_author a WHERE (w.author_id = a.id) AND (w.book_id = b.id) AND (w.sequence = 1)'
         )
 
     genre_list = Genre.objects.raw(
@@ -28,9 +28,107 @@ def book_search(request):
     return render(request, 'bookstore/book_search.html', 
         {'books': books_list,
          'genres': genre_list,
+         'currnet_genre': 'All',
          'title':'Book Search Page',
          'year':datetime.now().year,}
     )
+
+def book_search_top(request):
+    books_list = Book.objects.raw(
+            'SELECT * FROM bookstore_book b, bookstore_wrote w, bookstore_author a WHERE (w.author_id = a.id) AND (w.book_id = b.id) AND (w.sequence = 1) ORDER BY b.sales_rank LIMIT 10'
+        )
+
+    genre_list = Genre.objects.raw(
+            'SELECT * FROM bookstore_genre'
+    )
+
+    return render(request, 'bookstore/book_search.html', 
+        {'books': books_list,
+         'genres': genre_list,
+         'currnet_genre': 'All',
+         'title':'Book Search Page',
+         'top': True,
+         'year':datetime.now().year,}
+    )
+
+def genre_search(request, genre):
+    
+    if genre == 'All':
+        temp = ''
+    else:
+        temp = 'AND (b.book_genre_id = {})'.format(genre)
+
+    books_list = Book.objects.raw(
+        'SELECT * FROM bookstore_book b, bookstore_wrote w, bookstore_author a WHERE (w.author_id = a.id) AND (w.book_id = b.id) AND (w.sequence = 1) {}'.format(temp)
+    )
+
+    genre_list = Genre.objects.raw(
+            'SELECT * FROM bookstore_genre'
+    )
+
+    return render(request, 'bookstore/book_search.html', 
+        {'books': books_list,
+         'genres': genre_list,
+         'genre':genre,
+         'title':'Book Search Page',
+         'year':datetime.now().year,}
+    )
+
+def genre_search_sort(request, genre, sort):
+
+    temp = "SELECT * FROM bookstore_book b, bookstore_wrote w, bookstore_author a WHERE (w.author_id = a.id) AND (w.book_id = b.id) AND (w.sequence = 1) "
+
+    #Sort by different Ratings
+    if sort == '1Star':
+        temp = temp + 'AND (b.rating > 0.0) '
+    elif sort == '2Star':
+        temp = temp + 'AND (b.rating > 1.0) '
+    elif sort == '3Star':
+        temp = temp +  'AND (b.rating > 2.0) '
+    elif sort == '4Star':
+        temp = temp + 'AND (b.rating > 3.0) '
+    elif sort == '5Star':
+        temp = temp + 'AND (b.rating > 4.0) '
+
+    #Keeps the genre that was previously selected
+    if genre != 'All':
+        temp = temp + "AND (b.book_genre_id = {0})".format(genre)
+    
+    #If Sort by Book Title
+    if sort == 'Title':
+        temp = temp + "Order By b.book_title ASC"
+    #If Sort by book author name (last name)
+    elif sort == 'Author':
+        temp = temp + "Order By a.last_name ASC"
+    #If Sort by Price (Least to Greatest)
+    elif sort == 'PriceAsc':
+        temp = temp + "Order By b.price ASC"
+    #If Sort by Price (Greatest to Least)
+    elif sort == 'PriceDesc':
+        temp = temp + "Order By b.price DESC"
+    #If sort by Rating (Highest to Lowest)
+    elif sort == 'Rating':
+        temp = temp + "Order By b.rating DESC"
+    #If sort by Date Published
+    elif sort == 'PublishDate':
+        temp = temp + "Order By b.publish_date ASC"
+
+    books_list = Book.objects.raw(
+        '{0}'.format(temp)
+    )
+
+    genre_list = Genre.objects.raw(
+            'SELECT * FROM bookstore_genre'
+    )
+
+    return render(request, 'bookstore/book_search.html', 
+        {'books': books_list,
+         'genres': genre_list,
+         'genre':genre,
+         'title':'Book Search Page',
+         'year':datetime.now().year,}
+    )
+
 
 def login(request):
     assert isinstance(request, HttpRequest)
